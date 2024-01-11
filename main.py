a2e7e57f18e572126ed167e8cd6b2867d0c09c05
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-11 22:07:53 +0530

    Took the input from the user

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    while True:
+        print("***Welcome to Audio and Video downloader from Youtube***")
+        print("1.Download Video 2.Download Audio 3.Exit")
+        user_choice = input("Enter your choice (1/2/3): ")
+        
