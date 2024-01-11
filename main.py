744dbd55d9e64889e9ad917a4a2229e0bf77392f
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-11 22:15:52 +0530

    added main function

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ def main():
         print("***Welcome to Audio and Video downloader from Youtube***")
         print("1.Download Video 2.Download Audio 3.Exit")
         user_choice = input("Enter your choice (1/2/3): ")
-        
+        if user_choice == "1":
+            url = input("Enter url of the video: ")
+            downloadVideo(url)
+        elif user_choice == "2":
+            url = input("Enter url of the audio: ")
+            downloadAudio(url)
+        elif user_choice == "3":
+            print("***Thank you for using the app***")
+            return
+        else:
+            print("Enter a valid choice!!")
+
+main()
